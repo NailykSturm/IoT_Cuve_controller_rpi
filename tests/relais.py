a3d4c99ac080da297fd0429a9401614421683d67
NailykSturm <kykydesavoie@hotmail.com>
2024-03-08 15:08:25 +0100

    test_relais

---
+++ tests/relais.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO
+
+class GroveRelay(GPIO):
+    def __init__(self, pin):
+        super(GroveRelay, self).__init__(pin, GPIO.OUT)
+
+    def on(self):
+        self.write(1)
+
+    def off(self):
+        self.write(0)
+
+Grove = GroveRelay
+
+def main():
+    import sys
+    import time
+
+    if len(sys.argv) < 2:
+        print('Usage: {} pin'.format(sys.argv[0]))
+        sys.exit(1)
+
+    relay = GroveRelay(int(sys.argv[1]))
+
+    while True:
+        try:
+            relay.on()
+            time.sleep(1)
+            relay.off()
+            time.sleep(1)
+        except KeyboardInterrupt:
+            relay.off()
+            print("exit")
+            exit(1)      
+
+if __name__ == "__main__":
+    main()
